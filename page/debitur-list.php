6588d6eb36370fdea4b4366d6cf4a69bb76a4009
Mike Yates <mike.frankyates@gmail.com>
2016-04-21 15:27:46 +0800

    Added files via upload

---
+++ page/debitur-list.php
@@ -0,0 +1,25 @@
+<script src="js/accordion.js"></script>
+
+<div class="content-wrapper">
+
+<div><i class="fa fa-user"></i>Nasabah (234)<div class="add-button"><i class="fa fa-user-plus"></i>Nasabah Baru</div></div>
+    </br>
+
+    
+
+<button id="accordion-click" class="accordion"><div class="noinduk-box"> 0001</div>Yan Michael Frank Yates <div class="drop-arrow"><i id="drop-arrow" class="fa fa-chevron-down"></i></div></button>
+<div class="panel">
+  <p>Nama: Yan Michael Frank Yates</p>
+</div>
+
+<button class="accordion">Section 2</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
+
+<button class="accordion">Section 3</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
+
+</div>
---
+++ page/leftnav.php
@@ -0,0 +1,19 @@
+<div class="leftnav">
+    
+    <div class="leftnav-logo-wrap">
+    <div class="leftnav-title"><i class="fa fa-apple"></i> IndoBali Group</div><div class="leftnav-subtitle">Apple Reseller</div>
+    </div>
+        
+<div class="leftnav-menu">
+    <ul class="leftnav-ul">
+    <li class="leftnav-li"><i class="fa fa-home"></i>Dashboard</li>
+        <li><div class="seperator"></div></li>
+    <li class="leftnav-li"><i class="fa fa-user"></i>Nasabah<div class="total-info">234</div></li>
+        <li class="leftnav-li"><i class="fa fa-list"></i>Produk<div class="total-info">18</div></li>
+        <li><div class="seperator"></div></li>
+        <li class="leftnav-li"><i class="fa fa-list"></i>Karyawan<div class="total-info">9</div></li>
+    </ul>
+    
+    </div>
+
+</div>
