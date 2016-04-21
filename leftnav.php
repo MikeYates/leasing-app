af021e68670af1c5e5bc9c093859f413b347eb9c
Mike Yates <mike.frankyates@gmail.com>
2016-04-21 15:26:25 +0800

    Added files via upload

---
+++ leftnav.php
@@ -0,0 +1,5 @@
+<ul class="nav nav-pills nav-stacked leftnav">
+  <li role="presentation" class="active"><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+      <li role="presentation"><a href="#"><i class="fa fa-user"></i> Daftar Nasabah</a></li>
+  <li role="presentation"><a href="#"><i class="fa fa-list"></i> Daftar Produk</a></li>
+</ul>
